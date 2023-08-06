1d423f22624bf9cda56a699768f4eb56ccbf151c
Gnlow <gnlow@protonmail.com>
2023-08-06 17:41:20 +0900

    not working as expected

--- mod.ts
+++ mod.ts
@@ -1,22 +1,26 @@
 export type Conversions<T extends Record<string, unknown>> = {
-    [A in keyof T]?: {
-        [B in keyof T]?: (a: T[A]) => T[B]
+    [A in keyof T & string]?: {
+        [B in keyof T & string]?: (a: T[A]) => T[B]
     }
 }
 
 export const flow = <A, B, C>(aToB: (a: A) => B, bToC: (b: B) => C) => (a: A) => bToC(aToB(a))
 
-export class Convertible<T extends Record<string, unknown>> {
+export class Convertible<
+    T extends Record<string, unknown>,
+    C extends Conversions<T>,
+> {
     conversions
-    constructor(conversions: Conversions<T>) {
+    constructor(conversions: C) {
         this.conversions = conversions
     }
 
-    findPath<A extends keyof T, B extends keyof T>(a: A, b: B) {
+    findPath<A extends keyof T & string, B extends keyof T & string>(a: A, b: B)
+    : IsPathAvailable<T, C, A, B> {
         const aTo = this.conversions[a]
         if (aTo) {
             if (aTo[b]) {
-                return true
+                return true as any
             } else {
                 const findResult = Object.entries(aTo).find(([k]) => {
                     const kTo = this.conversions[k]
@@ -28,19 +32,58 @@ export class Convertible<T extends Record<string, unknown>> {
                     }
                 })
                 if (findResult) {
-                    return true
+                    return true as any
                 }
-                return false
+                return false as any
             }
         } else {
-            return false
+            return false as any
         }
     }
-    convert<A extends keyof T, B extends keyof T>(a: A, b: B, value: T[A]): T[B] {
+    convert<A extends keyof T, B extends keyof T>(a: A, b: B, value: T[A])
+    : IsPathAvailable<T, Conversions<T>, A, B> extends true ? T[B] : undefined {
         if (this.findPath(a, b)) {
-            return this.conversions[a]![b]!(value) as T[B]
+            return this.conversions[a]![b]!(value) as T[B] as any
         } else {
-            throw new Error("No path found")
+            return undefined as any
         }
     }
 }
+
+type IsPathAvailable<
+    T extends Record<any, unknown>,
+    C extends Conversions<T>,
+    A extends keyof T,
+    B extends keyof T,
+> = A extends keyof C
+    ? B extends keyof C[A]
+        ? true
+        : keyof C[A] extends any
+            ? 
+                (
+                    keyof C[A] extends keyof T
+                        ? IsPathAvailable<T, C, keyof C[A], B>
+                        : "LOL"
+                ) extends false ? false : true
+                /*
+                    (0 | 1) -> 1
+                    1 -> 1
+                    0 -> 0
+                 */
+            : never
+    : false
+
+type Test = IsPathAvailable<
+    {a: 1, b: 2, c: 3, d: 4},
+    {
+        a: {
+            b: (a: 1) => 2
+            d: (a: 1) => 4,
+        },
+        b: {
+            c: (b: 2) => 3,
+        },
+    },
+    "a",
+    "c"
+>
--- test.ts
+++ test.ts
@@ -1,21 +1,23 @@
 import { Convertible } from "./mod.ts"
 
+const o = {
+    json: { arr: (json:string) => JSON.parse(json) },
+    arr: {
+        json: (arr: (string | number)[]) => JSON.stringify(arr),
+        list: (arr: (string | number)[]) => arr.join("\n")
+    },
+    list: { arr: (list: string) => list.split("\n") }
+}
+
 const c = new Convertible<{
     json: string,
     list: string,
     arr: (string | number)[],
-}>(
-    {
-        json: { arr: JSON.parse },
-        arr: {
-            json: JSON.stringify,
-            list: arr => arr.join("\n")
-        },
-        list: { arr: list => list.split("\n") }
-    }
-)
+}, typeof o>(o)
 
 
 console.log(c.convert("json", "arr", "[1,2,3]"))
 console.log(c.convert("json", "list", "[1,2,3]"))
 console.log(c.convert("list", "json", "1\n2\n3"))
+
+c.findPath("json", "arr")
